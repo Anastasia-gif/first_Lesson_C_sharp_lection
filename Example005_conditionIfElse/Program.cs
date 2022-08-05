554d4fe9cbff118df2d39a94908fef9d8a56f822
Анастасия <nastenka_slastenka2022@mail.ru>
2022-08-05 23:58:32 +0800

    add Example 005 Condition If Else

---
+++ Example005_conditionIfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите имя пользователя :");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "настя"){
+    System.Console.WriteLine("Ураа, привет " + userName);
+}else{
+    System.Console.WriteLine("Привет, " + userName);
+}
