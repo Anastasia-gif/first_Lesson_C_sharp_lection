df9cadb4d5a8bebe78d3bbab0088b604057d0a21
Анастасия <nastenka_slastenka2022@mail.ru>
2022-08-06 00:30:59 +0800

    add Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 0;
+
+System.Console.WriteLine("Сколько у вас гирик? ");
+int countBols = Convert.ToInt32(Console.ReadLine());
+
+int [] weightBols = new int [countBols];
+
+int i = 0;
+while(i < weightBols.Length){
+    System.Console.WriteLine("Введите вес " + (i + 1) + "-ой гири");
+    int wB = Convert.ToInt32(Console.ReadLine());
+    weightBols[i] = wB;
+    i++;
+}
+
+for(int j = 0; j < weightBols.Length; j++){
+    if(weightBols[j] > max){
+        max = weightBols[j];
+    }
+}
+System.Console.WriteLine("Максимальный вес " + max);
