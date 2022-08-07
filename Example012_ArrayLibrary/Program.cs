525a21dbbd89432bd47b0e3e4216d410fae36c52
Анастасия <nastenka_slastenka2022@mail.ru>
2022-08-08 00:51:43 +0800

    add Example 012 Array Library

---
+++ Example012_ArrayLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿void fillArray(int [] collection){
+    int lenght = collection.Length;
+    int index = 0;
+
+    while(index < lenght){
+
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int [] col){
+    int count = col.Length;
+    int position = 0;
+
+    while(position < count){
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int [] collection, int find){
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count){
+        if(collection[index] == find){
+            position = index;
+            break;
+        }
+        index++;
+    }
+
+    return position;
+}    
+int [] array = new int[10];
+
+fillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 5);
+System.Console.WriteLine(pos);
+
+
