755b90bc5a18f7064a90ee9cd678afebb30998e9
Анастасия <nastenka_slastenka2022@mail.ru>
2022-08-07 21:56:34 +0800

    change Example 003 Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,7 @@
-﻿int numberA = new Random(1,10);
+﻿int numberA = new Random().Next(1,10);
 System.Console.WriteLine("Первое число " + numberA);
 
-int numberB = new Random(1,10);
+int numberB = new Random().Next(1,10);
 int sum = numberA + numberB;
 
 System.Console.WriteLine("Второе число " + numberB);
