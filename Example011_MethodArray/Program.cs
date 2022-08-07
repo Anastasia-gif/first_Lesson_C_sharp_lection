517c42a0c2ce4dbf238fd0339881dfda01fd6d86
Анастасия <nastenka_slastenka2022@mail.ru>
2022-08-07 23:48:41 +0800

    add Example 011 Method Array

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] array = {1, 12, 31, 4, 15, 16, 17, 18};
+int n = array.Length;
+int find = 17;
+int index = 0;
+
+while(index < n){
+    if(array[index] == find){
+        System.Console.WriteLine("index вашего элемента = " + index);
+        break;
+    }
+    index ++;
+}
