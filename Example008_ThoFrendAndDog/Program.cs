2cb93f335b516c95f564ad8e1bfc22e4149d985e
Анастасия <nastenka_slastenka2022@mail.ru>
2022-08-07 21:49:21 +0800

    add Example Tho Frends and Dog

---
+++ Example008_ThoFrendAndDog/Program.cs
@@ -0,0 +1,33 @@
+﻿int count = 0;
+
+System.Console.WriteLine("Введите дистанцию:");
+int distance = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите cкорость первого друга:");
+int firstFrendSpeed = Convert.ToInt32(Console.ReadLine());
+
+
+System.Console.WriteLine("Введите cкорость второго друга:");
+int secondFrendSpeed = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите cкорость собаки:");
+int dogSpeed = Convert.ToInt32(Console.ReadLine());
+
+int frend = 2;
+
+while(distance > 10){
+    int time;
+    if(frend == 1){
+        time = distance / (firstFrendSpeed + dogSpeed);
+        frend = 2;
+    }
+    else{
+        time = distance / (secondFrendSpeed + dogSpeed);
+        frend = 1;
+    }
+
+    distance = distance - (firstFrendSpeed + secondFrendSpeed) * time;
+    count += 1;
+}
+
+System.Console.WriteLine("Собака пробежит " + count + " раз");
